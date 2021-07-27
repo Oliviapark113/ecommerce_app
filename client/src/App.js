00749d3da4ad179cad12b643322f036dcb5d6b36
Olivia Park <oliviaypark113@gmail.com>
2021-07-27 09:30:21 -0400

    first client side update

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,64 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import "./index.css"
+import data from './data';
+
 
 function App() {
   return (
-    <div className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>Welcome to React</h2>
+    <>
+      <div className="App">
+
+        <div className="grid-container">
+          <header className="row">
+            <div>
+              <a href="/" className="brand">amazonia</a>
+            </div>
+            <div>
+              <a herf="/cart">Cart</a>
+              <a herf="/signin">Sign In </a>
+            </div>
+          </header>
+          <main>
+            <div className="row center">
+              {
+                data.products.map(product => (
+
+                  <div key ={product._id} className="card">
+                  <a href={`/product/${product._id}`}>
+  
+                    <img className="medium" 
+                    src={product.image} 
+                    alt={product.name} />
+                  </a>
+                  <div className="card-body">
+                  <a href={`/product/${product._id}`}>
+                  <h2>{product.name}</h2>
+                    </a>
+                    <div className="rating">
+                      <span><i className="fa fa-star"></i></span>
+                      <span><i className="fa fa-star"></i></span>
+                      <span><i className="fa fa-star"></i></span>
+                      <span><i className="fa fa-star"></i></span>
+                      <span><i className="fa fa-star"></i></span>
+                    </div>
+                <div className="price">${product.price} </div>
+                  </div>
+                </div>
+
+                ))
+              }
+
+
+            </div>
+          </main>
+          <footer className="row center">
+            All right reserved.
+          </footer>
+        </div>
+
+
       </div>
-      <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
-    </div>
+    </>
   );
 }
 
---
+++ client/src/data.js
@@ -0,0 +1,91 @@
+const data = {
+    products:[
+        {
+            _id:"1",
+            name:"Olivia's Tank Top",
+            category:"Tops",
+            image:"/images/t1.png",
+            price: 34,
+            brand:"Olivia's",
+            rating:3,
+            numReviews:10,
+            description: "Ultra Soft Ribbed Knit product"
+
+        },
+        {
+            _id:"2",
+            name:"Lules Bra Top",
+            category:"Tops",
+            image:"/images/a1.png",
+            price: 34,
+            brand:"Lules",
+            rating:4.5,
+            numReviews:8,
+            description: "Activewear Bra Top"
+
+        },
+        {
+            _id:"3",
+            name:"Lules Cross Front Top",
+            category:"Tops",
+            image:"/images/t2.png",
+            price: 44,
+            brand:"Lules",
+            rating:4,
+            numReviews:15,
+            description: "Off the shoulder Styling"
+
+        },
+        {
+            _id:"4",
+            name:"Olivia's ruffled Crop Top",
+            category:"Tops",
+            image:"/images/s1.png",
+            price: 54,
+            brand:"Olivia's",
+            rating:4,
+            numReviews:20,
+            description: "Ruffled Crop top with Lace up detail"
+
+        },
+        {
+            _id:"5",
+            name:"Olivia's Moto Jacket",
+            category:"Jacket",
+            image:"/images/j1.png",
+            price: 228,
+            brand:"Olivia's",
+            rating:5,
+            numReviews:30,
+            description: "Moto Jacket"
+
+        },
+        {
+            _id:"6",
+            name:"Lules Strap Dress",
+            category:"Dress",
+            image:"/images/d1.png",
+            price: 88,
+            brand:"Lules",
+            rating:4.5,
+            numReviews:30,
+            description: "Strap shoulder Bodycon Dress"
+
+        },
+        {
+            _id:"7",
+            name:"Jamies Shorts",
+            category:"Pants",
+            image:"/images/b1.png",
+            price: 68,
+            brand:"JNYC",
+            rating:4,
+            numReviews:30,
+            description: "White Shorts"
+
+        }
+    
+    ]
+}
+
+export default data;
